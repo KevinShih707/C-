2b1ea926a40089b8b1fdca16699ecdd07ea6c84f
KevinShih707 <d122784090@gmail.com>
2019-09-25 19:17:52 +0800

    hw1_finished 九九乘法表

---
+++ p76084300_hw1/p76084300_hw1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p76084300_hw1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 1;
+            do
+            {
+                x = 1;
+                do
+                {
+                    Console.Write("{0}*{1}={2} ", y, x, x * y);
+                    x++;
+                }
+                while (x < 10);
+                y++;
+                Console.WriteLine();
+            }
+            while (y < 10);
+            Console.Read();
+        }
+    }
+}
