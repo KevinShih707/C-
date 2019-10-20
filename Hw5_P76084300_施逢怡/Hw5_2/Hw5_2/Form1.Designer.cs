8980b7a191ca44871e93dad6092ebee9d0637ebe
KevinShih707 <d122784090@gmail.com>
2019-10-21 00:13:39 +0800

    hw5 finished

--- Hw5_P76084300_施逢怡/Hw5_2/Hw5_2/Form1.Designer.cs
+++ Hw5_P76084300_施逢怡/Hw5_2/Hw5_2/Form1.Designer.cs
@@ -28,21 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label_name = new System.Windows.Forms.Label();
+            this.label_password = new System.Windows.Forms.Label();
+            this.button_query = new System.Windows.Forms.Button();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.textBox_password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label_name
+            // 
+            this.label_name.AutoSize = true;
+            this.label_name.Location = new System.Drawing.Point(119, 76);
+            this.label_name.Name = "label_name";
+            this.label_name.Size = new System.Drawing.Size(98, 18);
+            this.label_name.TabIndex = 0;
+            this.label_name.Text = "使用者姓名";
+            // 
+            // label_password
+            // 
+            this.label_password.AutoSize = true;
+            this.label_password.Location = new System.Drawing.Point(119, 139);
+            this.label_password.Name = "label_password";
+            this.label_password.Size = new System.Drawing.Size(98, 18);
+            this.label_password.TabIndex = 1;
+            this.label_password.Text = "使用者密碼";
+            // 
+            // button_query
+            // 
+            this.button_query.Location = new System.Drawing.Point(278, 217);
+            this.button_query.Name = "button_query";
+            this.button_query.Size = new System.Drawing.Size(89, 65);
+            this.button_query.TabIndex = 2;
+            this.button_query.Text = "查詢";
+            this.button_query.UseVisualStyleBackColor = true;
+            this.button_query.Click += new System.EventHandler(this.button_query_Click);
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.Location = new System.Drawing.Point(267, 73);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(112, 29);
+            this.textBox_name.TabIndex = 3;
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(267, 136);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(112, 29);
+            this.textBox_password.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(760, 351);
+            this.ClientSize = new System.Drawing.Size(751, 353);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.button_query);
+            this.Controls.Add(this.label_password);
+            this.Controls.Add(this.label_name);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label_name;
+        private System.Windows.Forms.Label label_password;
+        private System.Windows.Forms.Button button_query;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.TextBox textBox_password;
     }
 }
 
--- Hw5_P76084300_施逢怡/Hw5_2/Hw5_2/Form1.cs
+++ Hw5_P76084300_施逢怡/Hw5_2/Hw5_2/Form1.cs
@@ -12,6 +12,9 @@ namespace Hw5_2
 {
     public partial class Form1 : Form
     {
+        UserRec rec;               // 建立UserRec結構型別的變數rec
+                                   // 建立Dictionary類別物件
+        Dictionary<string, UserRec> userDict = new Dictionary<string, UserRec>();
         public Form1()
         {
             InitializeComponent();
@@ -25,9 +28,6 @@ namespace Hw5_2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            UserRec rec;               // 建立UserRec結構型別的變數rec
-            // 建立Dictionary類別物件
-            Dictionary<string, UserRec> userDict = new Dictionary<string, UserRec>();
             rec.passwd = "123456";         // 指派結構變數rec成員資料
             rec.sex = true;
             rec.email = "cc@yljh.edu.tw";
@@ -42,6 +42,46 @@ namespace Hw5_2
             rec.sex = true;
             rec.email = "aaa";
             userDict.Add("施逢怡", rec);    // 加入第二筆資料到userDict物件中
+        }
+
+        private void button_query_Click(object sender, EventArgs e)
+        {
+            string name = textBox_name.Text;
+            string password = textBox_password.Text;
+            string input_name = name;
+            string fm;
+            if (!userDict.ContainsKey(name))  // 若userDict物件索引鍵不包含該姓名
+                MessageBox.Show("查無此人");
+            else                          // 若userDict物件索引鍵包含該姓名
+            {
+                string pw = password;         // 登入使用者密碼
+                if (userDict[name].passwd == pw)   // 若使用者密碼相符，顯示資料
+                {
+                    string input_fm="";
+                    if (userDict[name].sex)
+                        input_fm = "男";
+                    else
+                        input_fm = "女";
+                    string people = "";
+                    foreach (KeyValuePair<string, UserRec> kv in userDict)
+                    {
+                        name = kv.Key;
+                        if (kv.Value.sex)
+                            fm = "男";
+                        else
+                            fm = "女";
+                        pw = kv.Value.passwd;
+                        string mail = kv.Value.email;
+                        people+= (name+"\t"+fm+"\t"+pw+"\t"+mail+"\n");
+                    }
+                    MessageBox.Show("顯示"+ input_name + "相關資料\n"+ "性別 : "+ input_fm + "\nEmail："+ 
+                                    userDict[input_name].email+"\n"+ "\n列出所有使用者相關資料\n"+ 
+                                    "姓名\t性別\t密碼\tmail\n"+ "======\t====\t======\t==============\n"
+                                    +people);
+                }
+                else                                 // 若使用者密碼不符
+                    MessageBox.Show("密碼不對");
+            }
         }
     }
 }
