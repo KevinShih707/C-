fb0d0ac57dcdb8893d440985a44e94403b1b6900
KevinShih707 <d0911707007@gmail.com>
2019-12-03 16:59:41 +0800

    hw8

---
+++ Description/ch15/DataAdapterSql/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace DataAdapterSql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.book_number = new System.Windows.Forms.TextBox();
+            this.book_name = new System.Windows.Forms.TextBox();
+            this.book_author = new System.Windows.Forms.TextBox();
+            this.book_price = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(16, 15);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(622, 249);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(179, 467);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "新增";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 467);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "修改";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(382, 467);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "刪除";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // book_number
+            // 
+            this.book_number.Location = new System.Drawing.Point(179, 287);
+            this.book_number.Name = "book_number";
+            this.book_number.Size = new System.Drawing.Size(278, 25);
+            this.book_number.TabIndex = 4;
+            // 
+            // book_name
+            // 
+            this.book_name.Location = new System.Drawing.Point(179, 330);
+            this.book_name.Name = "book_name";
+            this.book_name.Size = new System.Drawing.Size(278, 25);
+            this.book_name.TabIndex = 5;
+            // 
+            // book_author
+            // 
+            this.book_author.Location = new System.Drawing.Point(179, 373);
+            this.book_author.Name = "book_author";
+            this.book_author.Size = new System.Drawing.Size(278, 25);
+            this.book_author.TabIndex = 6;
+            // 
+            // book_price
+            // 
+            this.book_price.Location = new System.Drawing.Point(179, 416);
+            this.book_price.Name = "book_price";
+            this.book_price.Size = new System.Drawing.Size(278, 25);
+            this.book_price.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(121, 290);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "書號";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(121, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "書名";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(121, 383);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "作者";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(121, 426);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 15);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "定價";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 519);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.book_price);
+            this.Controls.Add(this.book_author);
+            this.Controls.Add(this.book_name);
+            this.Controls.Add(this.book_number);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox book_number;
+        private System.Windows.Forms.TextBox book_name;
+        private System.Windows.Forms.TextBox book_author;
+        private System.Windows.Forms.TextBox book_price;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Hw8_P76084300_施逢怡/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace DataAdapterSql
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                 "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+                    "Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
+        //ID variable used in Updating and Deleting Record  
+        int ID = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                 "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+                    "Integrated Security=True"; //資料庫連接字串
+            SqlConnection db = new SqlConnection(cn);
+            db.Open();
+            SqlDataAdapter ap = new SqlDataAdapter
+                ("SELECT 書號,書名,作者,定價 FROM 書目 ORDER BY 定價 DESC", db);
+            DataSet ds = new DataSet();
+            ap.Fill(ds, "書目");
+            dataGridView1.DataSource = ds.Tables["書目"];
+            db.Close();
+        }
+
+
+        //新增
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (book_name.Text != "" && book_author.Text != "" && book_number.Text != "" && book_price.Text != "")
+            {/*
+                string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                     "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+                        "Integrated Security=True"; //資料庫連接字串
+                SqlConnection db = new SqlConnection(cn);
+                db.Open();
+
+                string a = book_number.Text;
+                string b = book_name.Text;
+                string c = book_author.Text;
+                string d = book_price.Text;
+                SqlDataAdapter ap = new SqlDataAdapter
+                ("INSERT INTO 書目(書號,書名,作者,定價) VALUES(a,b,c,d)", db);
+                DataSet ds = new DataSet();
+                ap.Fill(ds, "書目");
+                dataGridView1.DataSource = ds.Tables["書目"];*/
+                cmd = new SqlCommand("insert into 書目(書號,書名,作者,定價) values(@書號,@書名,@作者,@定價)", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@書號", book_number.Text);
+                cmd.Parameters.AddWithValue("@書名", book_name.Text);
+                cmd.Parameters.AddWithValue("@作者", book_author.Text);
+                cmd.Parameters.AddWithValue("@定價", book_price.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Inserted Successfully");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Provide Details!");
+            }
+        }
+
+        //Display Data in DataGridView  
+        private void DisplayData()
+        {
+            string cn = @"Data Source=(LocalDB)\MSSQLLocalDB;" +
+                 "AttachDbFilename=|DataDirectory|Database1.mdf;" +
+                    "Integrated Security=True"; //資料庫連接字串
+            SqlConnection db = new SqlConnection(cn);
+            db.Open();
+            SqlDataAdapter ap = new SqlDataAdapter
+                ("SELECT 書號,書名,作者,定價 FROM 書目 ORDER BY 定價 DESC", db);
+            DataSet ds = new DataSet();
+            ap.Fill(ds, "書目");
+            dataGridView1.DataSource = ds.Tables["書目"];
+            db.Close();
+        }
+
+        //Clear Data  
+        private void ClearData()
+        {
+            book_author.Text = "";
+            book_number.Text = "";
+            book_price.Text = "";
+            book_name.Text = "";
+        }
+        //修改
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (book_name.Text != "" && book_author.Text != "" && book_number.Text != "" && book_price.Text != "")
+            {
+                cmd = new SqlCommand("update 書目 set 書號=@書號,書名=@書名,作者=@作者,定價=@定價 where 書號=@書號", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@書號", book_number.Text);
+                cmd.Parameters.AddWithValue("@書名", book_name.Text);
+                cmd.Parameters.AddWithValue("@作者", book_author.Text);
+                cmd.Parameters.AddWithValue("@定價", book_price.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Record Updated Successfully");
+                con.Close();
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Update");
+            }
+        }
+
+        //刪除
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (book_number.Text != "")
+            {
+                cmd = new SqlCommand("delete 書目 where 書號=@書號", con);
+                con.Open();
+                cmd.Parameters.AddWithValue("@書號", book_number.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Record Deleted Successfully!");
+                DisplayData();
+                ClearData();
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Delete");
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
